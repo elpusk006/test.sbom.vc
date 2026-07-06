3fabcd27a2bee8664301dd12053f0cafff27d30e
ssyang <ssyang@elpusk.co.kr>
2026-07-06 14:46:28 +0900

    test 로 boot lib 제거

--- vc_sbom/vc_sbom.cpp
+++ vc_sbom/vc_sbom.cpp
@@ -1,13 +1,14 @@
 
 #include <iostream>
-#include <boost/filesystem.hpp>
+//#include <boost/filesystem.hpp>
 
 int main()
 {
     std::cout << "Hello World!\n";
-
+/*
 	boost::filesystem::path p = "/usr/local/bin";
 	std::cout << p << " path." << std::endl;
+	*/
 	return 0;
 }
 
